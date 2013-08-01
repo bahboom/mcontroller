ee62a032807a85b9fe5d79baf59ebab73c19a03d
Ming Liu <nongnongd@yahoo.com>
2013-07-31 20:29:28 -0400

    s

--- core/src/main/java/com/mcode/mcontroller/core/configs/MulticastConfig.java
+++ core/src/main/java/com/mcode/mcontroller/core/configs/MulticastConfig.java
@@ -3,7 +3,7 @@ package com.mcode.mcontroller.core.configs;
 public class MulticastConfig {
 	public static final String GROUP = "239.192.1.1";
 	public static final int PORT = 2222;
-	public static final int TTL = 1;
+	public static final int TTL = 3;
 	
 	private MulticastConfig() {
 		throw new UnsupportedOperationException();
