3247d0a34c0ad75c69693aec6dcaa20683b6db7c
Ming Liu <nongnongd@yahoo.com>
2013-07-31 22:29:58 -0400

    q

--- desktop/src/main/java/com/mcode/mcontroller/desktop/MControllerDesktop.java
+++ desktop/src/main/java/com/mcode/mcontroller/desktop/MControllerDesktop.java
@@ -1,12 +1,15 @@
 package com.mcode.mcontroller.desktop;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-
 import com.mcode.mcontroller.core.MController;
 
 public class MControllerDesktop {
-	public static void main (String[] args) {
+	public static void main (String[] args) throws UnknownHostException {
+		System.out.println(InetAddress.getLocalHost().getHostAddress());
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.useGL20 = true;
 		new LwjglApplication(new MController(), config);
